af759479dbcf3acb5149ae062a947bc31b88efae
CaroKris <ckhilpisch@gmail.com>
2021-03-17 18:12:30 -0500

    fixing the tag put route in api routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -39,17 +39,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.destroy ({
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!tagData) {
-      res.status(404).json({ message: 'No tag with that id exists!'});
-      return;
-    }
-
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -65,7 +55,7 @@ router.put('/:id', async (req, res) => {
       },
     });
     if (!tagData) {
-      res.status(404).jason({ message: "No tag found with that id"})
+      res.status(404).json({ message: "No tag found with that id"})
       return;
     }
     res.status(200).json(tagData);
